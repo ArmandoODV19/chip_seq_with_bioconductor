ab748e9268c99a425c0c4b779126d780b61378ae
armandios19 <fooseaov@gmail.com>
2022-07-21 10:04:44 -0500

    clase

--- scripts/capitulo_1.R
+++ scripts/capitulo_1.R
@@ -1,1 +1,18 @@
+# las secuencias mapeadas se suelen guaradar
+# en archivos BAM
+# la funcion readGAlignments() de la paqueteria GenomicAlignments
+# permite leer los archivos BAM
+
+BiocManager::install("GenomicAlignments")
+library(GenomicAlignments)
+
+readGAlignments()
+
+reads <- readGAlignments("archivo")
+seqnames(reads) # informacion sobre las lecturas del cromosoma
+start(reads) # inicio de secuencia
+end(reads) # fin de secuencia
+
+# computing coverage
+coverage(reads)
 
