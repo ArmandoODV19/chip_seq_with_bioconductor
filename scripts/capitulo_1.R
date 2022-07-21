44c655252634fc0631737ef807babe00bce2978b
armandios19 <fooseaov@gmail.com>
2022-07-21 10:10:04 -0500

    clase

--- scripts/capitulo_1.R
+++ scripts/capitulo_1.R
@@ -19,4 +19,15 @@ coverage(reads)
 # las unidades principales en el analisis en un experimento de
 # chip-seq son peak calls que sobresaltan regiones
 # del cromosoma con altas concentraciones de lecturas
+# estos picos se suelen guardar en archivos tipo BED
 
+# cargando archivos tipo BED
+library(rtracklayer)
+peaks <- import.bed("archivo")
+
+#obteniendo las coordenadas de los peak
+chrom(peaks)
+ranges(peaks)
+
+# extrayendo los puntajes de los peak
+score(peaks)
